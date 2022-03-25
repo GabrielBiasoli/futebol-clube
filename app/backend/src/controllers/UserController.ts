8c6e67bb0abf0ba596f652abb5265ce1d93536a2
Gabriel Biasoli <bielbiasoli33@icloud.com>
2022-03-25 16:58:05 -0300

    feat: change rescue from express-rescue to try and catch due to type problems

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,16 +1,21 @@
 import { Request, NextFunction, Response } from 'express';
-import rescue from 'express-rescue';
 import * as UserService from '../services/UserService';
 
 const MISSING_FIELDS = new Error('MISSING_FIELDS');
 
-export const login = rescue(
-  async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
-    if (!req.body.email || !req.body.password) next(MISSING_FIELDS);
+const validateLoginBody = (req: Request) => {
+  if (!req.body.email || !req.body.password) throw MISSING_FIELDS;
+};
+
+export const login = async (req: Request, _res: Response, next: NextFunction): Promise<void> => {
+  try {
+    validateLoginBody(req);
     const user = await UserService.login(req.body);
     req.body.user = user;
-    next();
-  },
-);
+  } catch (error) {
+    next(error);
+  }
+  next();
+};
 
 export default login;
