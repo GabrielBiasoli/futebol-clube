c6fd5e345654c150082affdbac105b6c56c61d1f
Gabriel Biasoli <bielbiasoli33@icloud.com>
2022-03-26 15:14:12 -0300

    test: add tests for '/clubs' route

--- app/backend/src/tests/clubs.spec.ts
+++ app/backend/src/tests/clubs.spec.ts
@@ -8,11 +8,33 @@ import { Response } from 'superagent';
 import { Model } from 'sequelize/types';
 import clubs = require('./data/clubs.json')
 
+chai.use(chaiHttp);
+const { expect } = chai;
+
 describe('Request GET method to route "/clubs" ', async () => {
+  let chaiHttpResponse: Response;
+
   describe('when clubs exist in database ', async () => {
 
-    it('return an array ', async () => {
+    before(async () => {
+      sinon
+        .stub(Club, "findAll")
+        .resolves(clubs as unknown as Model[])
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/clubs');
+    })
 
+    it('return an array ', async () => {
+      expect(chaiHttpResponse.body).to.be.an('array');
+    })
+
+    it('the array contains objects', async () => {
+      expect(chaiHttpResponse.body).to.be.an('array').of('objects')
+    })
+
+    it('the objects contain "id" and "clubname" properties', async () => {
+      expect(chaiHttpResponse.body[0]).to.have.all.keys('id', 'clubname');
     })
   })
 })
