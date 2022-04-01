2568074dead920385b49e58cc72a573a5e0d37af
Gabriel Biasoli <bielbiasoli33@icloud.com>
2022-04-01 15:28:11 -0300

    feat: start to develop functions for leaderboard stats

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,7 +1,14 @@
+import Match from 'src/database/models/Match';
+import Club from '../database/models/Club';
 import * as ClubService from './ClubService';
 import * as MatchService from './MatchService';
 
 const getTotalGameByClub = async () => {
   const clubs = await ClubService.getAll();
   const matchs = await MatchService.getAll();
+};
+
+const func = (matchs: Match[], { id, clubName }: Club) => {
+  const clubMatchs = matchs.filter((match) => match.homeTeam === id || match.awayTeam === id);
+  
 };
