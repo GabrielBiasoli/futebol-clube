0072b48d62ac5d2b8e3fc7c6d53be16ee60b685a
Gabriel Biasoli <bielbiasoli33@icloud.com>
2022-04-01 19:02:19 -0300

    feat: create function to get goals and match result

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -3,24 +3,40 @@ import Club from '../database/models/Club';
 import * as ClubService from './ClubService';
 import * as MatchService from './MatchService';
 
-const getTotalGameByClub = async () => {
-  const clubs = await ClubService.getAll();
-  const matchs = await MatchService.getAll();
-};
-
 const filterMatchsByClub = (matchs: Match[], { id, clubName }: Club) => {
   const clubMatchs = matchs.filter((match) => match.homeTeam === id || match.awayTeam === id);
 
   return { name: clubName, clubMatchs };
 };
 
+const getMatchResult = (goalsbalance: number) => {
+  if (goalsbalance > 0) return [1, 0, 0];
+  return goalsbalance === 0 ? [0, 1, 0] : [0, 0, 1];
+};
 
+const getMatchInfo = (clubMatchs: Match[], id: number) => clubMatchs.map((match) => {
+  const [teamSide, opositeSide] = match.homeTeam === id
+    ? ['homeTeam', 'awayTeam']
+    : ['awayTeam', 'homeTeam'];
+  const goalsFavor = match[`${teamSide}Goals` as keyof Match];
+  const goalsOwn = match[`${opositeSide}Goals` as keyof Match];
+  const goalsBalance = goalsFavor - goalsOwn;
+  const [victory, draw, loss] = getMatchResult(goalsBalance);
+  return {
+    goalsFavor,
+    goalsOwn,
+    goalsBalance,
+    victory,
+    draw,
+    loss,
+  };
+});
 
-const getGoals = (clubMatchs: Match[], id: number) => {
-  const matchsWithGoals = clubMatchs.map((match) => {
-    const [teamSide, opositeSide] = match.homeTeam === id ? ['homeTeam', 'awayTeam']
-      : ['awayTeam', 'homeTeam'];
-    const goalsFavor = match[`${teamSide}Goals` as keyof Match];
-    const goalsOwn = match[`${opositeSide}Goals` as keyof Match];
+const getTotalGameByClub = async () => {
+  const clubs = await ClubService.getAll();
+  const matchs = await MatchService.getAll();
+  const clubsWithMatchs = clubs.map((club) => filterMatchsByClub(matchs, club));
+  clubsWithMatchs.map(({ clubMatchs, name }) => {
+
   });
 };
