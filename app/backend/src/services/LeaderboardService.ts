5eb8f43ff6225fcc78170afd0f4ba265b97fa0aa
Gabriel Biasoli <bielbiasoli33@icloud.com>
2022-04-01 18:15:56 -0300

    feat: start to make map function for leaderboard items

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,4 +1,4 @@
-import Match from 'src/database/models/Match';
+import Match from '../database/models/Match';
 import Club from '../database/models/Club';
 import * as ClubService from './ClubService';
 import * as MatchService from './MatchService';
@@ -8,7 +8,19 @@ const getTotalGameByClub = async () => {
   const matchs = await MatchService.getAll();
 };
 
-const func = (matchs: Match[], { id, clubName }: Club) => {
+const filterMatchsByClub = (matchs: Match[], { id, clubName }: Club) => {
   const clubMatchs = matchs.filter((match) => match.homeTeam === id || match.awayTeam === id);
-  
+
+  return { name: clubName, clubMatchs };
+};
+
+
+
+const getGoals = (clubMatchs: Match[], id: number) => {
+  const matchsWithGoals = clubMatchs.map((match) => {
+    const [teamSide, opositeSide] = match.homeTeam === id ? ['homeTeam', 'awayTeam']
+      : ['awayTeam', 'homeTeam'];
+    const goalsFavor = match[`${teamSide}Goals` as keyof Match];
+    const goalsOwn = match[`${opositeSide}Goals` as keyof Match];
+  });
 };
